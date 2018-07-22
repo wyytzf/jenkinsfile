d095a3d95c84f8023d4644eeb0d7e8e91c52cdc1
wyytzf <wyytzf@163.com>
2018-07-22 23:26:55 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
-sh "echo script"
-sh "sudo docker exec -i testdocker /bin/sh -c 'chmod 777 /var/test_directory/script.sh'"
+sudo docker exec -i testdocker /bin/sh -c 'chmod 777 /var/test_directory/script.sh'
 // image may not have bash!
-sh "sudo docker exec -i testdocker /bin/sh -c '/var/test_directory/script.sh'"
+sudo docker exec -i testdocker /bin/sh -c '/var/test_directory/script.sh'
 }
