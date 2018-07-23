c188ebe028435f0fb24fedd5b2a8e45a638938f8
wyytzf <wyytzf@163.com>
2018-07-23 15:27:51 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-sudo docker exec -i testdocker /bin/sh -c 'cd /var/test_directory/answer && ./script.sh' > script_temp
+sudo docker exec -i testdocker /bin/sh -c 'cd /var/test_directory/answer && ./script.sh' # > script_temp
 pwd
