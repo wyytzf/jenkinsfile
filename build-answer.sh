d215af58c7c092e1060932373db19b02a2a4cb62
wyytzf <wyytzf@163.com>
2018-07-23 11:49:03 +0800

    Update build-answer.sh

--- build-answer.sh
+++ build-answer.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 curl $answer_path -o answer.zip
 unzip -o answer.zip -d answer
-sed '1c #!/bin/sh' ./answer/script.sh
+sed '1c #!/bin/sh' ./answer/script.sh > ./answer/script.sh
 sudo chmod 777 ./answer/script.sh
 sudo cp -f ./src/* ./answer/src
