2ca1ff5e10c22a5e9d4967107d1e04e1eb6f8239
wyytzf <wyytzf@163.com>
2018-07-23 12:05:05 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 // sudo docker exec -i testdocker /bin/sh -c 'sudo chmod 777 /var/test_directory/script.sh'
 // sudo docker exec -i testdocker /bin/sh -c '/var/test_directory/script.sh'
-sudo docker exec -it testdocker /bin/sh
+sudo docker exec -i testdocker /bin/sh
+/var/test_directory/script.sh
+exit
