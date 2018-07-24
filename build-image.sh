7e9bfbb523b31a82f01a17bb0c74cce4ee894217
wyytzf <wyytzf@163.com>
2018-07-24 10:58:58 +0800

    Update build-image.sh

--- build-image.sh
+++ build-image.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-sudo docker run --tty --name testdocker --detach $image
+sudo docker run --tty --name $image --detach $image
 sudo docker ps -a
-sudo docker cp ./answer testdocker:/var/test_directory
-sudo docker exec -i testdocker /bin/sh -c "ls /var/test_directory"
+sudo docker cp ./answer $image:/var/test_directory
+# sudo docker exec -i testdocker /bin/sh -c "ls /var/test_directory"
