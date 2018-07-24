c7de61c29a4acf92332f85c85058be7421598b96
wyytzf <wyytzf@163.com>
2018-07-24 13:19:58 +0800

    Update failure.sh

--- failure.sh
+++ failure.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 echo $env.LOCAL_ERROR
 echo $callback_url
+job_msg=cat /homework-master/script_temp
 curl -X POST \
     -F "status=5" \
     -F "msg=$LOCAL_ERROR" \
+    -F "job_msg=$job_msg" \
     $callback_url
