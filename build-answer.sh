ddb2a3398b50d7279e21e437e6c97a9afa47ba57
wyytzf <wyytzf@163.com>
2018-07-24 09:36:23 +0800

    Update build-answer.sh

--- build-answer.sh
+++ build-answer.sh
@@ -1,7 +1,17 @@
 #!/bin/sh
 curl $answer_path -o answer.zip
 unzip -o answer.zip -d answer
+# bash替换成sh
 sed '1c #!/bin/sh' ./answer/script.sh > ./answer/script.sh.tmp
 mv ./answer/script.sh.tmp ./answer/script.sh
 sudo chmod 777 ./answer/script.sh
+# 不同技术栈执行不同方法
 sudo cp -f ./src/* ./answer/src
+if [[$image == *"node"*]]
+then
+  echo "node"
+fi
+if [[$image == *"java"*]]
+then
+  echo "java"
+fi
