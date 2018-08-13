5567d233ff8f7589375c6d5625a80ceda686d0ed
wyytzf <wyytzf@163.com>
2018-08-13 18:02:20 +0800

    Update build-answer.sh

--- build-answer.sh
+++ build-answer.sh
@@ -12,7 +12,7 @@ then
   echo "In order to be compatible with the old version, please write 'rm -rf spec \n mv ./__answerBranch/spec ./spec' in your script file"
   sudo cp -rf ./ ../answer_temp
   sudo cp -rf ../answer_temp ./answer
-  sudo cp -rf ./__answerBranch ./script.sh
+  sudo cp -rf ./__answerBranch/script.sh ./script.sh
 else
   echo "not node"
 fi
